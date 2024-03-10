a3da323e2d06476ed9bcc076453e1678c537ec20
Adam Shater <adam.m.shater@gmail.com>
2024-03-09 23:21:35 -0500

    added animation to signup page

--- app/routes/_auth.signup.tsx
+++ app/routes/_auth.signup.tsx
@@ -6,7 +6,7 @@ import {
   redirect,
 } from "@vercel/remix";
 import classNames from "classnames";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { z } from "zod";
 import Spinner from "~/components/spinner";
 import { getUser, getUserById, signUp } from "~/features/auth/auth.server";
@@ -90,140 +90,157 @@ export default function SignUp() {
     navigation.state !== "idle" &&
     navigation.formData?.get("actionId") === "signup";
 
-  return data && data.success ? (
-    <Success />
-  ) : (
+  return (
     <div className="flex max-h-full flex-1 items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
-      <div className="w-full max-w-sm space-y-6">
-        <div className="flex flex-col items-start">
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign up
-          </h2>
-          <div className="flex items-center gap-1 justify-center">
-            <span>to join</span>
-            <Logo />
-          </div>
-        </div>
-
-        <Form className="flex flex-col gap-y-2" method="post">
-          <div className="relative flex flex-col gap-y-3 rounded-md">
-            <div>
-              <label htmlFor="username" className="sr-only">
-                Username
-              </label>
-
-              <div className="qoverflow-hidden  relative rounded-md">
-                <input
-                  id="username"
-                  name="username"
-                  required
-                  className="relative block w-full rounded border-0 px-3 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-primary-800 outline-none sm:text-sm sm:leading-6"
-                  placeholder="Username"
-                />
-              </div>
-
-              {data && "username" in data.data && data.data.username && (
-                <Alert>{data.data.username}</Alert>
-              )}
-            </div>
-
-            <div>
-              <label htmlFor="email-address" className="sr-only">
-                Email address
-              </label>
-
-              <div className="qoverflow-hidden  relative rounded-md">
-                <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="relative block w-full rounded  rounded-b-md border-0 px-3 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-primary-800 outline-none sm:text-sm sm:leading-6"
-                  placeholder="Email address"
-                />
-              </div>
-              {data && "email" in data.data && data.data.email && (
-                <Alert>{data.data.email.at(0)}</Alert>
-              )}
-            </div>
-
-            <div>
-              <label htmlFor="password" className="sr-only">
-                Password
-              </label>
-
-              <div className="relative">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="relative block w-full rounded border-0 px-3 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-800 outline-none sm:text-sm sm:leading-6"
-                  placeholder="Password"
-                />
-              </div>
-              {data && "password" in data.data && data.data.password && (
-                <Alert>{data.data.password}</Alert>
-              )}
-            </div>
-
-            <div>
-              <label htmlFor="password" className="sr-only">
-                Confirm Password
-              </label>
-
-              <div className="relative">
-                <input
-                  id="confirmPassword"
-                  name="confirmPassword"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="relative block w-full rounded border-0 px-3 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-800 outline-none sm:text-sm sm:leading-6"
-                  placeholder="Confirm password"
-                />
-              </div>
-              {data &&
-                "confirmPassword" in data.data &&
-                data.data.confirmPassword && (
-                  <Alert>{data.data.confirmPassword}</Alert>
-                )}
-            </div>
-          </div>
-
-          <div className="mt-4">
-            <div>
-              <button
-                name="actionId"
-                value="signup"
-                type="submit"
-                disabled={loading}
-                className={classNames(
-                  loading ? "mt-opacity-50" : "hover:bg-primary-700",
-                  "mt-4 flex h-10 w-full items-center justify-center rounded-md bg-primary-800 text-sm font-semibold leading-6 text-white  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-800"
-                )}
-              >
-                <div className="relative">
-                  {loading ? <Spinner className="w-5 h-5" /> : "Sign up"}
-                </div>
-              </button>
-            </div>
-          </div>
-        </Form>
-
-        <p className="text-center text-sm leading-6 text-gray-500">
-          Already a member?{" "}
-          <Link
-            prefetch="intent"
-            to="/login"
-            className="font-semibold text-primary-800 hover:text-primary-700"
+      <AnimatePresence initial={false} mode="wait">
+        {data?.success ? (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.3 }}
+            className="w-full h-full"
+            key="success"
           >
-            Log in here
-          </Link>
-        </p>
-      </div>
+            <Success />
+          </motion.div>
+        ) : (
+          <motion.div
+            key="form"
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            className="w-full max-w-sm space-y-6"
+          >
+            <div className="flex flex-col items-start">
+              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                Sign up
+              </h2>
+              <div className="flex items-center gap-1 justify-center">
+                <span>to join</span>
+                <Logo />
+              </div>
+            </div>
+
+            <Form className="flex flex-col gap-y-2" method="post">
+              <div className="relative flex flex-col gap-y-3 rounded-md">
+                <div>
+                  <label htmlFor="username" className="sr-only">
+                    Username
+                  </label>
+
+                  <div className="qoverflow-hidden  relative rounded-md">
+                    <input
+                      id="username"
+                      name="username"
+                      required
+                      className="relative block w-full rounded border-0 px-3 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-primary-800 outline-none sm:text-sm sm:leading-6"
+                      placeholder="Username"
+                    />
+                  </div>
+
+                  {data && "username" in data.data && data.data.username && (
+                    <Alert>{data.data.username}</Alert>
+                  )}
+                </div>
+
+                <div>
+                  <label htmlFor="email-address" className="sr-only">
+                    Email address
+                  </label>
+
+                  <div className="qoverflow-hidden  relative rounded-md">
+                    <input
+                      id="email-address"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      className="relative block w-full rounded  rounded-b-md border-0 px-3 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-primary-800 outline-none sm:text-sm sm:leading-6"
+                      placeholder="Email address"
+                    />
+                  </div>
+                  {data && "email" in data.data && data.data.email && (
+                    <Alert>{data.data.email.at(0)}</Alert>
+                  )}
+                </div>
+
+                <div>
+                  <label htmlFor="password" className="sr-only">
+                    Password
+                  </label>
+
+                  <div className="relative">
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      className="relative block w-full rounded border-0 px-3 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-800 outline-none sm:text-sm sm:leading-6"
+                      placeholder="Password"
+                    />
+                  </div>
+                  {data && "password" in data.data && data.data.password && (
+                    <Alert>{data.data.password}</Alert>
+                  )}
+                </div>
+
+                <div>
+                  <label htmlFor="password" className="sr-only">
+                    Confirm Password
+                  </label>
+
+                  <div className="relative">
+                    <input
+                      id="confirmPassword"
+                      name="confirmPassword"
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      className="relative block w-full rounded border-0 px-3 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-800 outline-none sm:text-sm sm:leading-6"
+                      placeholder="Confirm password"
+                    />
+                  </div>
+                  {data &&
+                    "confirmPassword" in data.data &&
+                    data.data.confirmPassword && (
+                      <Alert>{data.data.confirmPassword}</Alert>
+                    )}
+                </div>
+              </div>
+
+              <div className="mt-4">
+                <div>
+                  <button
+                    name="actionId"
+                    value="signup"
+                    type="submit"
+                    disabled={loading}
+                    className={classNames(
+                      loading ? "mt-opacity-50" : "hover:bg-primary-700",
+                      "mt-4 flex h-10 w-full items-center justify-center rounded-md bg-primary-800 text-sm font-semibold leading-6 text-white  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-800"
+                    )}
+                  >
+                    <div className="relative">
+                      {loading ? <Spinner className="w-5 h-5" /> : "Sign up"}
+                    </div>
+                  </button>
+                </div>
+              </div>
+            </Form>
+
+            <p className="text-center text-sm leading-6 text-gray-500">
+              Already a member?{" "}
+              <Link
+                prefetch="intent"
+                to="/login"
+                className="font-semibold text-primary-800 hover:text-primary-700"
+              >
+                Log in here
+              </Link>
+            </p>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
