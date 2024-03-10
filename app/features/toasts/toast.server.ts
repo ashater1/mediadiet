c74ecef960e82b58ac0771bbbae8a000408a8e7e
Adam Shater <adam.m.shater@gmail.com>
2024-03-09 19:19:18 -0500

    added functions to flash server side toasts in session cookies

---
+++ app/features/toasts/toast.server.ts
@@ -0,0 +1,38 @@
+import { getSession, commitSession } from "~/sessions.server";
+
+export type Toast = {
+  title: string;
+  description: string;
+};
+
+export async function setToast({
+  request,
+  response,
+  toast,
+}: {
+  request: Request;
+  response: Response;
+  toast: Toast;
+}) {
+  const session = await getSession(request.headers.get("Cookie"));
+
+  session.flash("toast", {
+    title: toast.title,
+    description: toast.description,
+  });
+
+  response.headers.append("Set-Cookie", await commitSession(session));
+}
+
+export async function getToast({
+  request,
+  response,
+}: {
+  request: Request;
+  response: Response;
+}) {
+  const session = await getSession(request.headers.get("Cookie"));
+  const toast = session.get("toast");
+  response.headers.append("Set-Cookie", await commitSession(session));
+  return toast;
+}
---
+++ app/sessions.server.ts
@@ -0,0 +1,10 @@
+import { createCookieSessionStorage } from "@vercel/remix";
+import { Toast } from "~/features/toasts/toast.server";
+
+export const { getSession, commitSession, destroySession } =
+  createCookieSessionStorage<never, { toast: Toast }>({
+    // a Cookie from `createCookie` or the CookieOptions to create one
+    cookie: {
+      name: "__session",
+    },
+  });
