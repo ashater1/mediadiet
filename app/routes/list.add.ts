8ca21cb313fa26e9db8ac0a792b0ee4248942ab9
Adam Shater <adam.m.shater@gmail.com>
2024-03-26 12:42:38 -0400

    new search route and updates to add route

--- app/routes/list.add.ts
+++ app/routes/list.add.ts
@@ -8,6 +8,8 @@ export async function action({ request }: ActionFunctionArgs) {
   const user = await getUserOrRedirect({ request, response });
   const formData = await request.formData();
   const submission = Object.fromEntries(formData);
+
+  // TODO - Make sure book release date is passed and added to db for book entries
   const result = AddToListSchema.safeParse(submission);
 
   if (!result.success) {
